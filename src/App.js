904c5a161417bf6ea01a7ee0f5915a65e9b62a3d
adudley78 <dudleyadam@gmail.com>
2019-07-02 15:04:22 -0400

    Install and set up react bootstrap

--- src/App.js
+++ src/App.js
@@ -7,17 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Adam Dudley's</h2>
+        <h1>Flatiron React-Redux Project</h1>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        />
       </header>
     </div>
   );
